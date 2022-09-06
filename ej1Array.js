c8030fe08f27233ab09e585c00a759b8dde65f6f
aeseb <sebastoledano@outlook.com>
2022-09-06 11:35:47 -0300

    script

--- ej1Array.js
+++ ej1Array.js
@@ -12,44 +12,46 @@
  */
 
 
-    let arrayBinario = [
-        1, 0, 0, 1, 0, 1, 1, 1,
-        1, 0, 0, 1, 0, 0, 0, 0,
-        1, 0, 0, 1, 0, 0, 1, 1,
-        1, 0, 0, 1, 1, 1, 1, 0
-    ]
+let arrayBinario = [
+    1, 0, 0, 1, 0, 1, 1, 1,
+    1, 0, 0, 1, 0, 0, 0, 0,
+    1, 0, 0, 1, 0, 0, 1, 1,
+    1, 0, 0, 1, 1, 1, 1, 0
+]
 
-function mostrarVector(vector){
-    
-    for (let i = 0; i < (vector.length); i++){
-        if (arrayBinario[i] / 1 == 0){
+/**
+ * Funcion que intercambia 0s y 1s y viceversa
+ * @param {Array} vector a modificar
+ */
+function modificarVector(vector) {
+    for (let i = 0; i < (vector.length); i++) {
+        if (arrayBinario[i] / 1 == 0) {
             arrayBinario[i] = 1;
         }
-        else{
+        else {
             arrayBinario[i] = 0;
-            }
-            console.log(vector[i]);    
-    }  
-    
-    /*No consigo que funcione.
-
-    let grilla = vector.length * vector[0].length;
-
-    for(let i = 0; i < vector.length; i++){
-
-        for(let j = 0; j < vector[i].length; j++){
-            console.log(vector[i][j] + " ");
         }
-            console.log(grilla, "\n");
-        } 
-        */
+    }
 }
 
-mostrarVector(arrayBinario);
+/**
+ * Muestro el array arrayBinario
+ */
+function mostrarVector(estado) {
+    console.log(estado, arrayBinario.join(', '));
+}
+
+/**
+ * Muestro en consola el array sin modificar y 
+ * modificado.
+ */
+mostrarVector("Antes    ");
+modificarVector(arrayBinario);
+mostrarVector("Despues  ");
 
 
-    
-            
-    
 
-    
+
+
+
+
